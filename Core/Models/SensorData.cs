04374b38d90f6adcd70519f73ee3220ad1d63a19
SDTrinity <mahimabishtausjobs@yahoo.com>
2023-06-29 16:32:42 +0530

    Add project files.

---
+++ Core/Models/SensorData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventDrivenAppDemo.Core.Models
+{
+    public class SensorData
+    {
+        public string Gate { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int NumberOfPeople { get; set; }
+        public string Type { get; set; }
+
+    }
+}
