834e18246f8422d23331e28ec48d5557ec3fdaa3
SDTrinity <mahimabishtausjobs@yahoo.com>
2023-06-29 17:13:56 +0530

    SensorData

---
+++ RealTimeDashBoard/RealTimeDashBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+Console.WriteLine("[*]              GREEN STADIUM REAL TIME DASHBOARD               [*]");
+Console.WriteLine("[*]                                                              [*]");
+Console.WriteLine("[*]                                                              [*]");
+channel.ExchangeDeclare(exchange: "SensorDataExchange", type: ExchangeType.Fanout);
+
+// declare a server-named queue
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName,
+                  exchange: "SensorDataExchange",
+                  routingKey: string.Empty);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] {message}");
+};
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ SensorSimulator/SensorSimulator.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using EventDrivenAppDemo.Core.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+
+Console.WriteLine("[*]              SENSOR DATA GENERATION SIMULATION               [*]");
+Console.WriteLine("[*]                                                              [*]");
+Console.WriteLine("[*]                                                              [*]");
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "SensorDataExchange", type: ExchangeType.Fanout);
+
+bool entryType = true;
+while (true)
+{
+    var message = GetMessage(entryType);
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "SensorDataExchange",
+                         routingKey: string.Empty,
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($"Sent {message}");
+    entryType = !entryType;
+    Thread.Sleep(5000);
+}
+
+static string GetMessage(bool entryType)
+{
+    var numberOfPeople = new Random();
+    int count = numberOfPeople.Next(0,100);
+    var gate = new Random();
+    int gateNumber = numberOfPeople.Next(1, 6);
+
+    var sensorData = new SensorData()
+    {
+        Gate = gateNumber.ToString(),
+        TimeStamp = DateTime.UtcNow,
+        NumberOfPeople = count,
+        Type = (entryType == true ? "enter": "leave")
+    };
+    return JsonConvert.SerializeObject(sensorData);
+}
